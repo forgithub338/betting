3d41dc5fef91fac9bfb804e1b675ccbcf194fe16
Alex <forgithub338@gmail.com>
2025-06-13 14:59:31 +0800

    esLint閉嘴

--- src/app/admin/rank/page.tsx
+++ src/app/admin/rank/page.tsx
@@ -27,7 +27,7 @@ export default function Rank() {
 
       const data = await res.json();
       alert(data.message);
-    } catch (err) {
+    } catch (_err) {
       alert("送出失敗，請稍後再試！");
     } finally {
       setIsSubmitting(false);
